843febfb3f4f79fa6db533098063f3df3ff3d9a0
won <whl0409@naver.com>
2026-03-01 18:02:11 +0900

    feat: add intl package for date formatting support

---
+++ lib/core/utils/date_util.dart
@@ -0,0 +1,7 @@
+import 'package:intl/intl.dart';
+
+class DateUtil {
+  static final DateFormat dayLabel = DateFormat('MMM d');
+  static final DateFormat time = DateFormat('HH:mm');
+  static final DateFormat full = DateFormat('yyyy-MM-dd HH:mm');
+}
